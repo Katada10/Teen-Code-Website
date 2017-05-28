874658b56023ef71de37ae2153ef9586bb3c37a7
Katada Freije <katadafreije@hotmail.com>
2017-05-28 16:07:19 +0300

    Fixed Styling of home

---
+++ TeenCodeWebsite/Models/Certification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeenCodeWebsite.Models
+{
+    public class Certification
+    {
+        public string Name { get; set; }
+        public string IMGURL { get; set; }
+    }
+}
