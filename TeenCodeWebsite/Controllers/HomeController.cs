46cb863732fd69c78f2c2bec276a2481d24a7a57
Katada Freije <katadafreije@hotmail.com>
2017-07-15 13:12:09 -0400

    Fixed emailing from gmil and outook, need to fix yahoo

--- TeenCodeWebsite/Controllers/HomeController.cs
+++ TeenCodeWebsite/Controllers/HomeController.cs
@@ -27,11 +27,42 @@ namespace TeenCodeWebsite.Controllers
             return View();
         }
 
-        public ActionResult Work()
+        [HttpPost]
+        public ActionResult Contact(ContactForm form)
         {
-            return View();
-        }
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    var msg = new MailMessage()
+                    {
+                        From = new MailAddress(form.Email),
+                        Subject = form.Subject,
+                        Body = form.Message
+                    };
 
-        
+                    msg.To.Add("teencodedev@gmail.com");
+
+                    var smtp = new SmtpClient();
+
+                    smtp.Host = ("gmail-smtp-in.l.google.com");
+                    smtp.Port = 25;
+                    smtp.EnableSsl = true;
+
+                    smtp.UseDefaultCredentials = true;
+
+                    smtp.Send(msg);
+                    ModelState.Clear();
+                    
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+
+            return View();
+        }        
     }
 }
