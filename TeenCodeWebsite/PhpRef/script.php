7dfa096d21a388dddb06653b1530fde067999a7c
Katada Freije <katadafreije@hotmail.com>
2017-08-05 17:39:38 +0300

    Finalized website, need to add welcome video

--- TeenCodeWebsite/PhpRef/script.php
+++ TeenCodeWebsite/PhpRef/script.php
@@ -1,10 +1,19 @@
 <?php
 require 'PHPMailerAutoload.php';
 
-$email = "katadafreije@yahoo.com";
+
+function _prev($string)
+{
+    echo htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
+}
+
+
+
+$email = $_POST["Email"];
+$name = $_POST["Name"];
 $to = "teencodedev@gmail.com";
-$message = "hello";
-$subject = "subject";
+$message = $_POST["Message"];
+$subject = $_POST["Subject"];
 
 $mail = new PHPMailer;
 $mail->isSMTP(true);
@@ -13,14 +22,17 @@ $mail->Port = 25;
 
 $mail->addAddress($to);
 $mail->Subject = $subject;
-$mail->msgHTML("Message From : ".$email."<br> Message: <br>".$message);
+$mail->msgHTML("Message From : ".$name."<br> Email: <br>".$email."<br> Subject: <br>".$subject."<br> Message: <br>".$message);
 
-if (!$mail->send()) {
-$error = "Mailer Error: " . $mail->ErrorInfo;
-echo '<p>'.$error.'</p>';
+
+$url = "http://teencodeweb.azurewebsites.net/Home/Contact";
+$errorUrl = "http://teencodeweb.azurewebsites.net/Shared/Error";
+
+
+if ($mail->send()) {
+    header('Location: '.$url);
 }
 else {
-echo '<p>Message sent!</p>';
-}
-
+    header('Location: '.$errorUrl);
+}   
 ?>
