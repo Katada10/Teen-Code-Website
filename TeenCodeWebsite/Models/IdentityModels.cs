bc76217e57bf15608a482538466adfcb213e0c45
Katada Freije <katadafreije@hotmail.com>
2017-08-06 15:16:51 +0300

    Removed all database connections

--- TeenCodeWebsite/Models/IdentityModels.cs
+++ TeenCodeWebsite/Models/IdentityModels.cs
@@ -20,10 +20,6 @@ namespace TeenCodeWebsite.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-
-        public DbSet<SMLink> SMLinks { get; set; }
-
-
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
